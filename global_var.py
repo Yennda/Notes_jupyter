7506ca90453a4753afba1f09a22881ff3d589326
Yennda <ja.bukacke@gmail.com>
2020-10-15 08:58:13 +0200

    ...

---
+++ global_var.py
@@ -0,0 +1,9 @@
+yellow = '#ffb200'
+red = '#DD5544'
+blue = '#0284C0'
+black = '#000000'    
+green = '#008000'
+gray = '#555555'
+purple = '#DD84C0'
+
+COLORS = [yellow, red, blue, black, green, gray, purple]
